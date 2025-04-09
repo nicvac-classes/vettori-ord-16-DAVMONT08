0d588855024e81190c706ab537c9fefaba6b3238
DAVMONT08 <davide.montella@marconibari.edu.it>
2025-04-09 22:16:53 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,91 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() {  
+    #include <iostream>
+    #include <string>
+    #include <ctime>
+    
+    using namespace std;
+    
+
+    string toString (double);
+    int toInt (string);
+    double toDouble (string);
+    
+    int main() {
+        srand(time(0));   
+        int i, n, j, t, v;
+        string s;
+        bool scambio;
+    
+        cout << "quante macchine hanno gareggiato?" << endl;
+        cin >> n;
+        int r[n];
+        string nomi[n];
+    
+        i = 0;
+        while (i < n) {
+            cout << "come si chiama la macchina?" << endl;
+            cin >> nomi[i];
+            v = rand() % n + 1;
+            r[i] = v;
+            j = 0;
+            while (j < 1) {
+                if (v == r[i]) {
+                    v = v + 1;
+                    if (v > n) {
+                        v = 1;
+                    }
+                    j = 0;
+                } else {
+                    j = j + 1;
+                }
+            }
+            r[i] = v;
+            i = i + 1;
+        }
+        scambio = true;
+        i = 0;
+        while (i <= n - 1 && scambio) {
+            scambio = false;
+            j = 0;
+            while (j <= n - 2 - i) {
+                if (r[j] > r[j + 1]) {
+                    t = r[j];
+                    r[j] = r[j + 1];
+                    r[j + 1] = t;
+                    s = nomi[j];
+                    nomi[j] = nomi[j + 1];
+                    nomi[j + 1] = s;
+                    scambio = true;
+                }
+                j = j + 1;
+            }
+            i = i + 1;
+        }
+        j = 0;
+        while (j < n) {
+            cout << "l'auto " << nomi[j] << " è arrivata nella posizione numero " << r[j] << endl;
+            j = j + 1;
+        }
+        return 0;
+    }
+    
+    
+    string toString (double value) 
+        stringstream temp;
+        temp << value;
+        return temp.str();
+    }
+    
+    
+       
+
+
+
+
 }
+
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
